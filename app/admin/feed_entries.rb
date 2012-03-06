3bd120503b3de550dfa2feb7ae4876faee87bb87
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2012-03-05 20:04:19 -0600

    entries controller does not need a modified index to filter by the news feed

--- app/admin/feed_entries.rb
+++ app/admin/feed_entries.rb
@@ -3,13 +3,4 @@ ActiveAdmin.register FeedEntry do
     title :name # Calls #my_title on each resource
     body  :summary  # Calls #my_body on each resource
   end
-
-  controller do
-    def index
-      index! do|format|
-        @search = FeedEntry.search(params[:q])
-        @feed_entries = @search.page(params[:page]).per(20)
-      end
-    end
-  end
 end
