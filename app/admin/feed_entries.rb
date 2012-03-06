84bf6182e72b36d79fb0b19bd16337b2e9a378b4
Antonio Chavez <cavjzz@gmail.com>
2012-03-06 16:27:16 -0600

    Deleted new feed entries button from admin/feed_entries

--- app/admin/feed_entries.rb
+++ app/admin/feed_entries.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register FeedEntry do
+  actions :index, :show
+
   index :as => :blog do
     title :name # Calls #my_title on each resource
     body  :summary  # Calls #my_body on each resource
