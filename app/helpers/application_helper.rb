e8ee10847f11ad4c1729894ba4349274a48edcab
Alejandro Espinoza Bautista <alejandro@hotmail.com>
2012-03-07 19:43:16 -0600

    change some styles link to buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
     message = entry.visible? ? "Hide" : "Show"
     link_to message, toggle_visible_admin_news_feed_feed_entry_path(entry.feed, entry), :method => :put
   end
+  def title_layout(object)
+    unless object.nil?
+      if object.is_a?Array
+        object.first.class.name
+      else
+        object.class 
+      end
+    end
+  end
+
+  def set_nav_active(path)
+   "current" if current_page?(path)
+  end
+
 end
