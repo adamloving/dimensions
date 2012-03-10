07f6f49bac53fde394bff16fef977e4c41ddfeb0
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2012-03-10 14:18:22 -0600

    modify the config file to see if it extends the libraries

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Initialize the rails application
+RailsBootstrap::Application.initialize!
+
 ENV['LD_LIBRARY_PATH'] ||="/usr/lib"
 ENV['LD_LIBRARY_PATH'] +=":/app/lib/native"
 
-# Initialize the rails application
-RailsBootstrap::Application.initialize!
