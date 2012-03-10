1ac25244f6aa775b79bc636d01ab051b9dc946c0
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2012-03-10 10:41:37 -0600

    [#26148327] add the libtidy binary to allow the scrapping

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['LD_LIBRARY_PATH'] ||="/usr/lib"
+ENV['LD_LIBRARY_PATH'] +=":/app/lib/native"
+
 # Initialize the rails application
 RailsBootstrap::Application.initialize!
