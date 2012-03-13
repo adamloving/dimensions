7390c0b2a7726fae7f37612f878578469cd45381
Federico Ramallo <framallo@gmail.com>
2012-03-13 14:19:10 -0600

    added feeds and uniqueness

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+#
+
+NewsFeed.create :name => 'King5 Home', :url => 'http://www.king5.com/home/index.rss'
+NewsFeed.create :name => 'King5 Home', :url => 'http://www.king5.com/home/index.rss'
+NewsFeed.create :name => 'King5 Home', :url => 'http://www.king5.com/home/index.rss'
+NewsFeed.create :name => 'GeekWire' , :url => 'http://feeds.feedburner.com/geekwire?format=xml'
+NewsFeed.create :name => 'Seattle Business News' , :url => 'http://feeds.bizjournals.com/bizj_seattle?format=xml'
+NewsFeed.create :name => 'Seattle Mag' , :url => 'http://seattlemag.com/rss.xml'
+NewsFeed.create :name => 'Seattle Weekly' , :url => 'http://www.seattleweekly.com/syndication/issue/'
+NewsFeed.create :name => 'TechFlash' , :url => 'http://feeds.feedburner.com/TechFlash'
+NewsFeed.create :name => 'kitsapsun' , :url => 'http://www.kitsapsun.com/rss/headlines/news/'
+NewsFeed.create :name => 'KUOW' , :url => 'http://feeds2.feedburner.com/KUOW'
+NewsFeed.create :name => 'kplu.org' , :url => 'http://kplu.org/rss.xml'
+NewsFeed.create :name => 'pnwlocalnewsall' , :url => 'http://feeds.feedburner.com/pnwlocalnewsall'
+NewsFeed.create :name => 'sample1 ' , :url => 'http://www.xfruits.com/jonsayer/?id=105421'
+NewsFeed.create :name => 'seattletimes' , :url => 'http://seattletimes.nwsource.com/rss/seattletimes.xml'
+
