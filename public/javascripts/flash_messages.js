229cc0530fe827b1833070f44fb53ae21930642f
Alejandro Espinoza Bautista <alejandro@hotmail.com>
2012-03-12 18:57:23 -0600

    flash notice feed when process a newsfeed on show view

--- public/javascripts/flash_messages.js
+++ public/javascripts/flash_messages.js
@@ -2,10 +2,12 @@ function flashMessage(type, message){
   var flashesContainer = $(".flashes");
   flashesContainer.html("<div class='flash flash_" + type + "'>" + message + "</div>");
 }
-$(function(){
+
+function hideMessage(){
   if ($('.flash').length){
     setTimeout(function(){
       $(".flash").remove();
     },4000)
   }
-});
+}
+$(hideMessage);
