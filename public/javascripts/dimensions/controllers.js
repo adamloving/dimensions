1d26484bb0f6799e3e245b5aba02fe4d4c4c7b86
Alejandro Espinoza Bautista <alejandro@hotmail.com>
2012-03-19 09:35:36 -0600

    added controller for event search on searchify client

---
+++ public/javascripts/dimensions/controllers.js
@@ -0,0 +1,35 @@
+$(window).ready(function(event){
+
+ $('#search').change(function(e){
+    Router.handleRequest("search");
+ });
+ 
+ 
+
+ $.address.change(function(e){
+   Router.handleRequest(e,"search");
+ })
+
+});
+
+function SearchController(){
+  this.init=function(){
+    console.log("instancia de search");
+    window.dimensions = new Dimensions.Renderer();
+    window.dimensions.template =$("#item-template").template("NewsStreams");
+    window.dimensions.element = "#stream"; // render on this element
+  }
+  this.indexAction = function(){
+    window.dimensions.bind("loadItems",function(){
+    $(this.element).empty().append("<p>Loading...</p>")
+    query = window.filter.getQuery();
+    if(!query.q){
+      query.q = "seattle";
+      query.fetch = '*';
+    }
+      window.searchify.search(query);
+    });
+    window.dimensions.loadItems();
+  }
+
+}
---
+++ public/javascripts/dimensions/renderer.js
@@ -0,0 +1,58 @@
+$(function(){
+  $namespace("Dimensions").Renderer = function(){};
+  $namespace("Dimensions").Renderer.prototype ={
+    render: function(){
+      if($(this.element).length){
+        $(this.element).empty();
+        if(this.data.results.length > 0){
+          $.tmpl(this.template,{items:this.data.results}).appendTo(this.element);
+          clearMap();
+
+          $.each(this.data.results, function(i, r) {
+            var breakingNews, displayMarker, latitude, longitude, _ref, _ref2;
+            //longitude = 47.5 + Math.random() / 4.0;
+            //latitude = -122.0 - Math.random() * 0.5;
+            if(r.variables){
+              lat_long = eval('('+r.variables.replace(/=>/g,":")+')');
+              latitude = lat_long[0];
+              longitude = lat_long[1];
+              breakingNews = (_ref = Math.random() < .2) != null ? _ref : {
+                1: 0
+              };
+              displayMarker = (_ref2 = Math.random() < .4) != null ? _ref2 : {
+                1: 0
+              };
+              if (displayMarker > 0) {
+                if (breakingNews < 1) {
+                  return addMarker(longitude, latitude);
+                } else {
+                  return addBreakingNewsMarker(longitude, latitude);
+                }
+              }
+            }
+          });
+
+        }else{
+          $("#stream").append('<div class="no-results"><h1>Sorry, I find nothing :-(</h1>' + '<p>Searched for: ' + window.filter.getQueryAsHtml() + '</p></div>');
+                              //$.each(this.data.results,function(i,r){
+                              //console.log("latitude for each topic");
+                              //});
+        }
+        FB.XFBML.parse();//reload facebook events 
+      }else{
+        console.log(this.element+"  does not exist!");
+      }
+      this.unbind("loadItems");
+    },
+    onLoadComplete: function(response){
+      this.data = response;
+      this.render(this.element);
+    },
+    bind: function(name, fn){
+      Dimensions.Renderer.method(name, fn);
+    },
+    unbind:function(name){
+      Dimensions.Renderer.method(name,null);
+    }
+  }
+});
