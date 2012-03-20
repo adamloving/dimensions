ca4884968068f37c2c3ba9a53d3ae807dadcdf98
Inaki De La Madrid <ignacio.delamadrid@gmail.com>
2012-03-20 09:12:34 -0600

    added association between news feed and entity to save the location for the newsfeed

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,11 @@
 class Entity < ActiveRecord::Base
   belongs_to  :feed_entry
+  belongs_to  :news_feed
+
   serialize   :serialized_data, Hash
 
+  # we fucked up with the naming of the STI names.
   Entity.inheritance_column= "ruby_type"
+
   scope :locations, where(:type => "location")
 end
--- app/models/news_feed.rb
+++ app/models/news_feed.rb
@@ -2,14 +2,20 @@ require 'dimensions/netutils'
 
 class NewsFeed < ActiveRecord::Base
   include Dimensions::Netutils
+  has_one   :location, :class_name => Entity
+  has_many  :entries, :class_name =>  FeedEntry, :dependent => :restrict
 
-  has_many :entries, :class_name =>  FeedEntry, :dependent => :restrict
+
+  attr_accessor :location_values
 
   validates :name, :url, presence: true
+
   validates_uniqueness_of :name, :url
 
 
   before_save :url_connection_valid? unless Rails.env.test?
+
+  before_save :build_location
 
   def load_entries
     entries = FeedEntry.update_from_feed(self.url)
@@ -26,5 +32,43 @@ class NewsFeed < ActiveRecord::Base
     entries.each do |entry|
       entry.next
     end
+  end
+
+  
+  def location_values
+    @location_values ||= {:serialized_data => {}}
+  end
+
+  def address=(address)
+    location_values[:name] = address
+  end
+
+  def address
+    return "" if self.location.nil?
+    self.location.name 
+  end
+
+  def location_latitude
+    return "" if self.location.nil?
+    self.location.serialized_data['latitude']
+  end
+
+  def location_latitude=(latitude)
+    location_values[:serialized_data]['latitude'] = latitude
+  end
+
+  def location_longitude
+    return "" if self.location.nil?
+    self.location.serialized_data['longitude']
+  end
+
+  def location_longitude=(longitude)
+    location_values[:serialized_data]['longitude'] = longitude
+  end
+
+  private
+
+  def build_location
+    self.location = Entity.new(location_values.merge(:type => 'location'))
   end
 end
