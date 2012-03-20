20aaebd3c52879dcda403cafff29f1245ab85f8d
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-03-20 14:50:12 -0700

    added more validations to nerify-address

--- public/javascripts/news_feeds/verify-address.js
+++ public/javascripts/news_feeds/verify-address.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
   $("#verify_address").click(function(){ 
     var myAddressQuery = $("#news_feed_address").val();
 
@@ -16,27 +16,31 @@ $(function(){
           var index = 0;
 
           _.each(results, function(result){
-            locations += "<p data-location-index=" + index + ">" +  result.formatted_address +"<a href='#' class='pick_location'>Select this location</a></p>";
+            locations += "<p data-location-index=" + index + ">" +  result.formatted_address +"<a href='#' class='pick_location' data-location='"+result.formatted_address+"'>Select this location</a></p>";
             index++;
             return locations;
           });
 
           $('.address_results').data('locations', results);
           $('.address_results').html("<p>Is any of the following locations correct? </p>" + locations);
+          $('.errors_for_address').empty();
         }
     });
     return false;
   });
 
   $(".pick_location").live("click", function(){
-    var index       = $(this).parents("p").data('location-index');
-    var locations   = $('.address_results').data('locations');
-    var latitude    = locations[index].geometry.location.lat();
-    var longitude   = locations[index].geometry.location.lng();
+    var index       = $(this).parents("p").data('location-index')
+       ,locations   = $('.address_results').data('locations')
+       ,latitude    = locations[index].geometry.location.lat()
+       ,longitude   = locations[index].geometry.location.lng();
 
     $('#news_feed_location_longitude').val(longitude);
     $('#news_feed_location_latitude').val(latitude);
     $(".subm input").css("display","initial");
+    var location = $(this).data('location');
+    $("#news_feed_address").val(location);
+
   });
 
   $(function(){
@@ -45,6 +49,19 @@ $(function(){
         $(".subm input").css("display","none");
       }
     });
+  });
+
+  $('#news_feed_address').keydown(function(e){
+
+    if($(this).val()==""){
+      $(".subm input").css("display","none");
+      $(".address_results").empty()
+      $('.errors_for_address').empty();
+    }else if(e.keyCode == 13){
+      $('#verify_address').click();
+    }else
+      $('#verify_address').click();
+
   });
 
 
