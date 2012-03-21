4d80f3d6c032a8f72f0d69a2bf58c15918a1f6d9
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-21 15:35:51 -0600

    added config for resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,5 @@
-require '../resque.yml'
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env = ENV['RAILS_ENV'] || 'development'
+
+resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+Resque.redis = resque_config[rails_env]
