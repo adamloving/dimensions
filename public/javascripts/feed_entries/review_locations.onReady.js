e5a1797e0b6dc16a74c9154ffdbce86521a43132
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-26 18:56:07 -0600

    fixed code to show and hide small image ajax loader

--- public/javascripts/feed_entries/review_locations.onReady.js
+++ public/javascripts/feed_entries/review_locations.onReady.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $("a.location").click(function(e) {
-    $('')
+    e.preventDefault();
+    var ajax_loader_small = $($($(this)[0]).parents('tr').find('.ajax_image')[0]).find(':first-child')[0];
+    $(ajax_loader_small).show();
     url = $(this).attr('href');
     var self = $(this);
 
@@ -15,17 +17,12 @@ $(document).ready(function(){
         column =  current.parent();
         current.remove();
         column.prepend(current)
+      },
+      complete: function(){
+        $(ajax_loader_small).hide();
       }
     });
     return false;
   });
 
-  $('.ajax_loading_small')
-  .hide()  // hide it initially
-  .ajaxStart(function() {
-    $(this).show();
-  })
-  .ajaxStop(function() {
-    $(this).hide();
-  });
 });
