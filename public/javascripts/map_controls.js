bd386138db20a0b512587d7de482f5fa4827a540
Inaki De La Madrid <ignacio.delamadrid@gmail.com>
2012-04-17 13:24:05 -0500

    show default location always when geolocating if the browser is mozilla

--- public/javascripts/map_controls.js
+++ public/javascripts/map_controls.js
@@ -34,6 +34,8 @@ function getNorthEastCoordinates(map)
 
 function centerMapToCurrentLocation()
 {
+    renderMapCentered(new google.maps.LatLng('47.614496', '-122.332077'));
+
     $('#map').gmap({ 'zoom': 9 });
     //Firefox sucks! When selecting 'not now' from the geolocation permissions im firefox, it does not get  in the error callback
     navigator.geolocation.getCurrentPosition(function(position) {
