1a3aa39e03b21eb31668277e63d9183aabb782d4
pairing <pairing@tangosource.com>
2012-07-13 00:22:32 +0000

    Indexed the social ranking to searchify
    Signed-off-by: Antonio Chavez <cavjzz@gmail.com>

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -171,7 +171,7 @@ class FeedEntry < ActiveRecord::Base
       doc_variables = { 0 => location["latitude"],
                         1 => location["longitude"],
                         2 => self.published_at.to_i,
-                        3 => self.ranking}
+                        3 => self.social_ranking}
 
       fields = {:url        => self.url, 
                 :timestamp  => self.published_at.to_i,
