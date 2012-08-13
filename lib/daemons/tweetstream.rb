c48dd0c7253c93c090771e1c6ca59040717b4c38
Antonio Chavez <cavjzz@gmail.com>
2012-08-13 15:20:17 -0500

    Deleted tweetstream pid from commit list

--- lib/daemons/tweetstream.rb
+++ lib/daemons/tweetstream.rb
@@ -6,6 +6,8 @@ ENV["RAILS_ENV"] ||= "production"
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 require 'tweetstream'
 
+logger = Logger.new("#{Rails.root}/log/tweetstream.log")
+
 $running = true
 Signal.trap("TERM") do 
   $running = false
@@ -14,8 +16,10 @@ end
 while($running) do
   # Replace this with your code
     TweetStream::Client.new.track("@dimensions") do |status|
-       puts "#{status.id} : #{status.text}; Coincidencias: #{"\033[32m 1\033[0m" if FeedEntry.where(:url => status.entities.urls[0]["expanded_url"]) } \n"
-       FeedEntry.add_tweet(status.entities.urls)
-       puts "=======================================================================================================================\n"
+      logger << status.text
+      logger << "#{status.id} : #{status.text}; Coincidencias: #{"\033[32m 1\033[0m" if FeedEntry.where(:url => status.entities.urls[0]["expanded_url"]) } \n"
+      FeedEntry.add_tweet(status.entities.urls)
+      logger << "=======================================================================================================================\n"
     end
+    logger.close
 end
