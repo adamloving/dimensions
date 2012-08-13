2e6c43435a02d003aa3539635b602f4def80aa07
Antonio Chavez <cavjzz@gmail.com>
2012-08-13 15:50:08 -0500

    Fixed nil error

--- lib/daemons/tweetstream.rb
+++ lib/daemons/tweetstream.rb
@@ -17,9 +17,11 @@ while($running) do
   # Replace this with your code
     TweetStream::Client.new.track("@dimensions") do |status|
       logger << status.text
-      logger << "#{status.id} : #{status.text}; Coincidencias: #{"\033[32m 1\033[0m" if FeedEntry.where(:url => status.entities.urls[0]["expanded_url"]) } \n"
-      FeedEntry.add_tweet(status.entities.urls)
-      logger << "=======================================================================================================================\n"
+      unless status.entities.urls.empty?
+        logger << "#{status.id} : #{status.text}; Coincidencias: #{"\033[32m 1\033[0m" if FeedEntry.where(:url => status.entities.urls[0]["expanded_url"]) } \n"
+        FeedEntry.add_tweet(status.entities.urls)
+      end
+      logger << "\n=======================================================================================================================\n"
     end
     logger.close
 end
