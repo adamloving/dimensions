15baa73dd8faeb29768123947fb5a499afcfa6a8
Antonio Chavez <cavjzz@gmail.com>
2012-08-22 19:27:10 -0500

    Process only indexed entries

--- app/workers/facebook_counter.rb
+++ app/workers/facebook_counter.rb
@@ -2,9 +2,11 @@ class FacebookCounter
   @queue = :feed_entry
   def self.perform
     FeedEntry.find_each do |entry|
-      entry.update_facebook_stats
-      entry.calculate_social_rank
-      entry.re_index
+      if entry.indexed?
+        entry.update_facebook_stats
+        entry.calculate_social_rank
+        entry.re_index
+      end
     end
   end
 end
